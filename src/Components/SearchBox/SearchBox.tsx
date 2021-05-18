0d17256948dbcaa9d81f94e138d1510f35699dcd
Karan Sanjeev <yo@alphaman.me>
2021-05-18 19:56:25 +0530

    Add debouncing on search

--- src/Components/SearchBox/SearchBox.tsx
+++ src/Components/SearchBox/SearchBox.tsx
@@ -1,16 +1,28 @@
 import cx from 'classnames'
 import { connectSearchBox } from 'react-instantsearch-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ChangeEventHandler, useState } from 'react'
 
 const SearchBox = connectSearchBox(({ currentRefinement, refine, isSearchStalled }) => {
+    const [timer, setTimer] = useState<NodeJS.Timeout>(setTimeout(() => {}, 1))
+    const [search, setSearch] = useState(currentRefinement)
+
+    const controlSearch: ChangeEventHandler<HTMLInputElement> = (event) => {
+        const value = event.currentTarget.value
+
+        clearTimeout(timer)
+        setTimer(setTimeout(() => refine(value), 150))
+        setSearch(value)
+    }
+
     return (
         <div className={cx("control has-icons-left has-icons-right", {'is-loading is-medium': isSearchStalled})}>
             <input 
                 type="search" 
                 className="input is-medium" 
                 placeholder="Enter something"
-                value={currentRefinement}
-                onChange={ev => refine(ev.currentTarget.value)} />
+                value={search}
+                onChange={controlSearch} />
             <span className="icon is-left">
                 <FontAwesomeIcon icon={["fas", "search"]} />
             </span>
