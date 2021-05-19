72ce0ef0a4234b707ab92596da5f3f1b0210b020
Karan Sanjeev <yo@alphaman.me>
2021-05-19 15:39:49 +0530

    Show pagination only when required

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -2,30 +2,34 @@ import cx from 'classnames';
 import { connectPagination } from 'react-instantsearch-dom'
 
 const CustomPagination = connectPagination(({ currentRefinement, nbPages, refine, createURL }) => {
-    return (
-        <nav className="pagination">
-            <ul className="pagination-list">                
-            {new Array(nbPages).fill(null).map((_, index) => {
-                const page = index + 1;
+    if (nbPages > 1) {
+        return (
+            <nav className="pagination">
+                <ul className="pagination-list">                
+                {new Array(nbPages).fill(null).map((_, index) => {
+                    const page = index + 1;
+    
+                    return (
+                        <li key={index}>
+                            <a
+                                href={createURL(page)}
+                                className={cx("pagination-link", {'is-current': currentRefinement === page})}
+                                onClick={event => {
+                                    event.preventDefault();
+                                    refine(page);
+                                }}
+                            >
+                                {page}
+                            </a>
+                        </li>
+                    );
+                })}
+                </ul>
+            </nav>
+        )
+    }
 
-                return (
-                    <li key={index}>
-                        <a
-                            href={createURL(page)}
-                            className={cx("pagination-link", {'is-current': currentRefinement === page})}
-                            onClick={event => {
-                                event.preventDefault();
-                                refine(page);
-                            }}
-                        >
-                            {page}
-                        </a>
-                    </li>
-                );
-            })}
-            </ul>
-        </nav>
-    )
+    return <div></div>
 })
 
 export default CustomPagination
