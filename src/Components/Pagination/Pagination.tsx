362c149b96a2b003d9e08c3f35de722c3dd810ff
Karan Sanjeev <yo@alphaman.me>
2021-05-19 18:05:46 +0530

    Scroll to top after clicking pagination

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import cx from 'classnames';
+import cx from 'classnames'
 import { connectPagination } from 'react-instantsearch-dom'
 
 const CustomPagination = connectPagination(({ currentRefinement, nbPages, refine, createURL }) => {
@@ -7,7 +7,7 @@ const CustomPagination = connectPagination(({ currentRefinement, nbPages, refine
             <nav className="pagination">
                 <ul className="pagination-list">                
                 {new Array(nbPages).fill(null).map((_, index) => {
-                    const page = index + 1;
+                    const page = index + 1
     
                     return (
                         <li key={index}>
@@ -15,8 +15,9 @@ const CustomPagination = connectPagination(({ currentRefinement, nbPages, refine
                                 href={createURL(page)}
                                 className={cx("pagination-link", {'is-current': currentRefinement === page})}
                                 onClick={event => {
-                                    event.preventDefault();
-                                    refine(page);
+                                    event.preventDefault()
+                                    window.scrollTo({ top: 0, behavior: 'smooth' })
+                                    refine(page)
                                 }}
                             >
                                 {page}
