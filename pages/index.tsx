8a7ee4fc14ae50ea13e70a378217d9a29eb4128d
Karan Sanjeev <yo@alphaman.me>
2021-05-20 22:19:23 +0530

    Migrated index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
 import Head from 'next/head'
+import Navbar from '../Components/Sidenav/Navbar'
+import SearchBox from '../Components/Searchbox/Searchbox'
+import HitsComponent from '../Components/Hit/HitComponent'
+import Pagination from '../Components/Pagination/Pagination'
+import { InstantSearch, Stats } from 'react-instantsearch-dom'
 
 export default function Home() {
     return (
@@ -8,6 +13,53 @@ export default function Home() {
                 <meta name="description" content="Search through all sanfoundry's MCQ in milliseconds. Filter through subjects, topics and even degrees" />
                 <title>Choicez | Search through 10 thousand MCQs in seconds</title>
             </Head>
+            <InstantSearch indexName="mcqs" searchClient={searchClient}>
+                <section className="hero is-small is-link">
+                    <div className="hero-body">
+                        <div className="container has-text-centered">
+                            <h1 className="title">{process.env.REACT_APP_NAME}</h1>
+                            <h2 className="subtitle">Search through all sanfoundry's MCQ in milliseconds</h2>
+                            <SearchBox />
+                            <div className="mt-4">
+                                <Stats />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <div className="container is-fluid">
+                    <div className="columns mt-5">
+                        <div className="column is-10">
+                            <HitsComponent />
+                        </div>
+                        <div className="column is-2">
+                            <Navbar />
+                        </div>
+                    </div>
+                </div>
+                <footer className="footer">
+                    <div className="container is-fluid">
+                        <div className="columns is-centered is-vcentered">
+                            <div className="column is-narrow-desktop">
+                                <Pagination />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="content has-text-centered mt-5">
+                        <p>
+                            <strong>{process.env.REACT_APP_NAME}</strong>, an open source software by <a href="https://alphaman.me">Karan Sanjeev</a>.
+                    </p>
+                        <p>
+                            Data in this website is solely owned by <a href="https://sanfoundry.com/">Sanfoundry</a>
+                        </p>
+                        <p>
+                            <a href={process.env.REACT_APP_BACKEND_GITHUB}>Backend Code</a> | {' '}
+                            <a href={process.env.REACT_APP_FRONTEND_GITHUB}>Client Code</a> | {' '}
+                            <a href="https://github.com/smartclash/">GitHub</a> | {' '}
+                            <a href="https://twitter.com/xXAlphaManXx">Twitter</a>
+                        </p>
+                    </div>
+                </footer>
+            </InstantSearch>
         </div>
     )
 }
