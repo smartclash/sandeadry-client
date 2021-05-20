46aef978da486cc2ce4b8a33e9f6ed369210e207
Karan Sanjeev <yo@alphaman.me>
2021-05-21 00:22:50 +0530

    Add fontawesome support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import 'bulma'
 import type { AppProps } from 'next/app'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
