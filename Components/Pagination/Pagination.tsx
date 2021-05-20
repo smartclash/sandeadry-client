3b9b5dfe0dd8665062817bf72760fc355bb2d947
Karan Sanjeev <yo@alphaman.me>
2021-05-20 22:01:12 +0530

    Added pagination

---
+++ Components/Pagination/Pagination.tsx
@@ -0,0 +1,37 @@
+import cx from 'classnames'
+import { connectPagination } from 'react-instantsearch-dom'
+
+const CustomPagination = connectPagination(({ currentRefinement, nbPages, refine, createURL }) => {
+    if (nbPages > 1) {
+        return (
+            <nav className="pagination">
+                <ul className="pagination-list">                
+                {new Array(nbPages).fill(null).map((_, index) => {
+                    const page = index + 1
+    
+                    return (
+                        <li key={index}>
+                            <a
+                                href={createURL(page)}
+                                className={cx("pagination-link", {'is-current': currentRefinement === page})}
+                                onClick={event => {
+                                    event.preventDefault()
+                                    window.scrollTo({ top: 0, behavior: 'smooth' })
+                                    refine(page)
+                                }}
+                            >
+                                {page}
+                            </a>
+                        </li>
+                    );
+                })}
+                </ul>
+            </nav>
+        )
+    }
+
+    return <div></div>
+})
+
+export default CustomPagination
+
